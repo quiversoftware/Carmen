571c78f602db6d942c11dbd26267e759918d8c7d
Ernesto <quiversoftwaremanager@gmail.com>
2020-06-20 06:13:13 +0200

    First commit

---
+++ components/core/AppBar.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-app-bar
+    v-scroll="onScroll"
+    :color="!isScrolling ? 'transparent' : 'white'"
+    fixed
+    :flat="isScrolling ? false : true"
+  >
+    <v-slide-x-transition>
+      <v-img
+        v-if="showLogo"
+        :src="require('@/assets/logo.png')"
+        class="shrink"
+        contain
+        height="50"
+      />
+    </v-slide-x-transition>
+
+    <v-spacer />
+
+    <social-media />
+  </v-app-bar>
+</template>
+
+<script>
+import SocialMedia from '@/components/SocialMedia.vue';
+
+export default {
+  name: 'AppBar',
+
+  components: {
+    SocialMedia,
+  },
+
+  data: () => ({
+    showLogo: false,
+    isScrolling: false,
+  }),
+
+  methods: {
+    onScroll() {
+      const offset = window.pageYOffset
+      this.isScrolling = offset > 50
+      this.showLogo = offset > 200
+    },
+  },
+}
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-app light>
+    <AppBar/>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <v-footer absolute app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+import AppBar from '@/components/core/AppBar.vue'
+
+export default {
+  components: { AppBar },
+  comments: {
+    AppBar,
+  },
+
+  data() {
+    return {}
+  },
+}
+</script>
